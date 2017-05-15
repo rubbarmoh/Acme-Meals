841c2fc19e26173f97d192c0743a2f8b8589bad0
Felipe Javier Garcia da Silva <felgarda@alum.us.es>
2017-05-15 17:26:58 +0200

    Creadas las vista de lista de mealORders y display de mealOrders

--- Acme-Meals/src/main/java/repositories/MealOrderRepository.java
+++ Acme-Meals/src/main/java/repositories/MealOrderRepository.java
@@ -1,12 +1,18 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.MealOrder;
 
 @Repository
 public interface MealOrderRepository extends JpaRepository<MealOrder, Integer> {
+	
+	@Query("select mo from MealOrder mo where mo.user.id=?1")
+	Collection<MealOrder> findByUser(int userId);
 
 }
